1a3a17f4326758cac6c5e91085c2b10e1de47eec
peng-ling <paulengling@Pauls-MacBook-Air.local>
2016-02-25 00:17:49 +0100

    mixin pattern async confusion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+//google maps start
+var initMap = function() {
+
+  var myLatLng = {
+    lat: 50.0716683,
+    lng: 8.2309231
+  };
+  //https://addyosmani.com/resources/essentialjsdesignpatterns/book/#mixinpatternjavascript
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 8
+  });
+
+  // wait for loaded and add markers
+  google.maps.event.addListener(map, 'idle', function() {
+    console.log('LOADED');
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+  return this;
+};
+//google maps end
+
+var xxx = function() {
+  this.yyy = 'yyy';
+  initMap.call(this);
+};
+
+xxx.prototype = Object.create(initMap.prototype);
+
+var fireMap = function() {
+  return fff = new xxx();
+};
+
 var places = [{
   name: "Ristorante Pizzeria Da Balbi",
   lat: 50.0716683,
@@ -22,13 +62,10 @@ var locationsViewModel = function() {
 
   self.filter = ko.observable('');
 
-  //
-  //filter the items using the filter text
   self.filterLocations = ko.computed(function() {
     var filter = self.filter().toLowerCase();
-    if (!filter || filter === ' ') {
+    if (!filter || filter === '') {
       return self.Locations();
-      console.console.log('gashjdgashdg');
     } else {
       return self.Locations().filter(function(item) {
         return RegExp(filter).test(item.name.toLowerCase());
@@ -38,24 +75,3 @@ var locationsViewModel = function() {
 };
 
 ko.applyBindings(new locationsViewModel());
-
-function initMap() {
-
-  var myLatLng = {
-    lat: 50.0716683,
-    lng: 8.2309231
-  };
-
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    scrollwheel: false,
-    zoom: 8
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
-}
