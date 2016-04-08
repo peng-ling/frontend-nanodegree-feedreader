a9b9adfc6ba9163a675f1692137a786738470e77
peng-ling <paulengling@Pauls-MacBook-Air.local>
2016-04-08 19:18:40 +0200

    final revison to submit for review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,13 +8,16 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
+
 $(function() {
-  console.log("Testing");
+
   /* This is our first test suite - a test suite just contains
    * a related set of tests. This suite is all about the RSS
    * feeds definitions, the allFeeds variable in our application.
    */
+
   describe('RSS Feeds', function() {
+
     /* This is our first test - it tests to make sure that the
      * allFeeds variable has been defined and that it is not
      * empty. Experiment with this before you get started on
@@ -22,12 +25,14 @@ $(function() {
      * allFeeds in app.js to be an empty array and refresh the
      * page?
      */
+
     it('are defined', function() {
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
 
   });
+
   /* TODO: Write a test that loops through each feed
    * in the allFeeds object and ensures it has a URL defined
    * and that the URL is not empty.
@@ -96,18 +101,47 @@ $(function() {
   });
 
   /* TODO: Write a new test suite named "Initial Entries" */
+  describe('Initial Entries', function() {
 
-  /* TODO: Write a test that ensures when the loadFeed
-   * function is called and completes its work, there is at least
-   * a single .entry element within the .feed container.
-   * Remember, loadFeed() is asynchronous so this test will require
-   * the use of Jasmine's beforeEach and asynchronous done() function.
-   */
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
 
-  /* TODO: Write a new test suite named "New Feed Selection"
+    var originalTimeout;
 
-      /* TODO: Write a test that ensures when a new feed is loaded
-       * by the loadFeed function that the content actually changes.
-       * Remember, loadFeed() is asynchronous.
-       */
+    beforeEach(function(done) {
+      loadFeed(0, done);
+    });
+
+    it('after load feed there should be at least one entry', function(done) {
+      expect($('.feed').find('.entry').length).not.toBe(0);
+      done();
+    });
+
+  });
+
+  /* TODO: Write a new test suite named "New Feed Selection"*/
+  describe('New Feed Selection', function() {
+
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+
+    beforeEach(function(done) {
+      loadFeed(0, done);
+    });
+
+    var oldFeedContent = $('.feed').find('.entry').text();
+
+    it('when a new feed is loaded the content actually changes', function(done) {
+      loadFeed(1, done);
+      expect($('.feed').find('.entry').text()).not.toBe(oldFeedContent);
+    });
+
+  });
+
 }());
